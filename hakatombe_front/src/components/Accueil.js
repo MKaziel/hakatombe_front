62ea3903e16c90c8965cdce9bf685cdb571dd493
MKaziel <mkaziel@hotmail.com>
2020-11-29 18:29:48 +0100

    resolve prob 2

--- hakatombe_front/src/components/Accueil.js
+++ hakatombe_front/src/components/Accueil.js
@@ -2,44 +2,42 @@ import React, { Component } from "react";
 import axios from 'axios';
 
 class Accueil extends Component {
-    static state = {
-        post: {},
-    }; 
+    state = {
+        data:[],
+    };
+
+    getData = function(){
+        var baseUrl = 'http://localhost:3000';
+        return axios.get(baseUrl+"/users")
+        .then(function (response) {
+            return response.data;
+        });
+    }
 
     // GET the data
     // Récupere les données des gens qui ont rempli le formulaire d'équipe
     // On récupère le nom le prenom l'@ mail et le projet proposé
-    componentDidMount() {
-        var baseUrl = 'http://localhost:3000';
-        let currentComponent = this;
-        axios.get(baseUrl+"/users")
-        .then(function (response) {
-            currentComponent.setState({
-                post: response,
-            });
-        });
+    async componentDidMount(){
+        const response = await fetch('http://localhost:3000/users');
+        const json = await response.json();
+        this.setState({data: json});
     }
 
-    render() {
-        let array = null;
-        console.log(this.state);
-        if (array !== null){
-            return (
-                <div>
-                    <h4>Hackamathon information</h4>
-                    <ul>
-                        {array.map(item => {return <li>{item}</li>;})}
-                    </ul>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <h4>Hackamathon information</h4>
-                </div>
-            );
-        }
-        
+    render() {   
+        let currentComponent = this;
+        console.log(currentComponent.state.data);        
+        return (
+            <div>
+                <h4>Hackathon information</h4>
+                <ul id="tab">
+                    {currentComponent.state.data.map(item => {
+                        return(<li>
+                            {item.email}: {item.fname}
+                            </li>)
+                    })}
+                </ul>
+            </div>
+        );        
     }
 }
 
