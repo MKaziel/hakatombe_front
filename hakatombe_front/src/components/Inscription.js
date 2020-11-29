548ffb16b475d98be9d70ca51a9dde3627d84d40
MKaziel <mkaziel@hotmail.com>
2020-11-29 23:05:44 +0100

    push merge file

--- hakatombe_front/src/components/Inscription.js
+++ hakatombe_front/src/components/Inscription.js
@@ -1,94 +1,126 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-class Inscription extends Component{  
-
+class Inscription extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: []
-        }
-    }
-    componentDidMount(){
-        fetch('http://localhost:3000/users')
-        .then(response => response.json())
-        .then(res => {
-            if(res && res.data) {
-                this.setState({ users:[...this.state.users, ...res.data]})
-            }
-        })
+            users: [],
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    renderUsers(){
-      if(this.state.users.length<= 0){
-        return <div>Loading..</div>
-      }
-      else{
-        return this.users.map((val,key) => {
-            return <div key={key}> {val.fname} | {val.lname} | {val.email} | {val.password} </div>
+    handleChange(event) {
+        this.setState({ users: event.target.value });
+    }
+
+    addToFormulaire = (event) => {
+        event.preventDefault();
+        this.setState({
+            users: event.target.value,
         });
-      }
-    }
 
+        axios
+            .post("http://localhost:3000/users", {
+                users: this.state.users,
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-
-
-        render(){
-            return(
+    render() {
+        return (
             <div className="container">
-              {this.renderUsers()}
                 <form>
-                  <div className="form-group col-md-4">
-                    <label htmlFor="InputFirsName">First name</label>
-                    <input className="form-control" type="text" id="inputFname" placeholder="First name"/>
-                  </div>
-        
-                  <div className="form-group col-md-4">
-                    <label htmlFor="InputLastName">Last name</label>
-                    <input className="form-control" type="text" id="inputLname" placeholder="Last name"/>
-                  </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="InputFirsName">First name</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            id="inputFname"
+                            placeholder="First name"
+                            onChange={this.handleChange}
+                        />
+                    </div>
 
-                  <div className="form-group col-md-4">
-                    <label htmlFor="inputEmail">Email address</label>
-                    <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp"/>
-                  </div>
-                  <div className="form-group col-md-4">
-                    <label htmlFor="inputPassword">Password</label>
-                    <input type="password" class="form-control" id="inputPassword"/>
-                  </div>
-        
-                <div className="form-group col-md-4">
-                    <label htmlFor="inputState">School</label>
-                    <select id="inputLocation" class="form-control">
-                    <option selected>Choose school</option>
-                    <option>Paris</option>
-                    <option>Saint-Quentin en Yvelines</option>
-                    <option>Marne-la-Vallée</option>
-                    </select>
-                </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="InputLastName">Last name</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            id="inputLname"
+                            placeholder="Last name"
+                            onChange={this.handleChange}
+                        />
+                    </div>
 
-                <div className="form-group col-md-4">
-                    <label htmlFor="inputState">Team</label>
-                    <select id="inputLocation" class="form-control">
-                    <option selected>Choose a team</option>
-                    <option>Les nulls</option>
-                    <option>Les nous</option>
-                    <option>Les vous</option>
-                    </select>
-                </div>
-        
-                 
-        
-                  
-        
-                  <button type="submit" class="btn btn-primary">Register</button>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputEmail">Email address</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="inputEmail"
+                            aria-describedby="emailHelp"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputPassword">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="inputPassword"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputState">School</label>
+                        <select
+                            id="inputLocation"
+                            className="form-control"
+                            onChange={this.handleChange}
+                        >
+                            <option selected>Choose school</option>
+                            <option>Paris</option>
+                            <option>Saint-Quentin en Yvelines</option>
+                            <option>Marne-la-Vallée</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputState">Team</label>
+                        <select
+                            id="inputLocation"
+                            className="form-control"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        >
+                            <option selected>Choose a team</option>
+                            <option>Team 1</option>
+                            <option>Team 2</option>
+                            <option>Team 3</option>
+                        </select>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        onClick={this.addToFormulaire}
+                    >
+                        Register
+                    </button>
                 </form>
             </div>
-            );
-        }
-        }
+        );
+    }
+}
 
-        /*function register(e){
+/*function register(e){
             e.preventDefault();
             let request = {
               fname : document.getElementById('inputFname').value,
@@ -108,11 +140,7 @@ class Inscription extends Component{
             })
   };*/
 
-        
-  
-
 export default Inscription;
-
 
 /*
 
--- hakatombe_front/src/components/inscription.js
+++ hakatombe_front/src/components/inscription.js
@@ -1,96 +1,126 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-class Inscription extends Component{  
-
+class Inscription extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: []
-        }
+            users: [],
+        };
         this.handleChange = this.handleChange.bind(this);
-
     }
 
-    handleChange(event){
-      this.setState({users: event.target.value});
+    handleChange(event) {
+        this.setState({ users: event.target.value });
     }
 
-    addToFormulaire = event =>{
-      event.preventDefault();
-      this.setState({
-        users: event.target.value
-      })
+    addToFormulaire = (event) => {
+        event.preventDefault();
+        this.setState({
+            users: event.target.value,
+        });
 
-      axios.post('http://localhost:3000/users', {
-          users: this.state.users
-      })
-      .then(response =>  {
-        console.log(response)
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    }
+        axios
+            .post("http://localhost:3000/users", {
+                users: this.state.users,
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-
-
-
-        render(){
-            return(
+    render() {
+        return (
             <div className="container">
                 <form>
-                  <div className="form-group col-md-4">
-                    <label htmlFor="InputFirsName">First name</label>
-                    <input className="form-control" type="text" id="inputFname" placeholder="First name"  onChange={this.handleChange}/>
-                  </div>
-        
-                  <div className="form-group col-md-4">
-                    <label htmlFor="InputLastName">Last name</label>
-                    <input className="form-control" type="text" id="inputLname" placeholder="Last name"  onChange={this.handleChange}/>
-                  </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="InputFirsName">First name</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            id="inputFname"
+                            placeholder="First name"
+                            onChange={this.handleChange}
+                        />
+                    </div>
 
-                  <div className="form-group col-md-4">
-                    <label htmlFor="inputEmail">Email address</label>
-                    <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp"  onChange={this.handleChange}/>
-                  </div>
-                  <div className="form-group col-md-4">
-                    <label htmlFor="inputPassword">Password</label>
-                    <input type="password" className="form-control" id="inputPassword" onChange={this.handleChange}/>
-                  </div>
-        
-                <div className="form-group col-md-4">
-                    <label htmlFor="inputState">School</label>
-                    <select id="inputLocation" className="form-control"  onChange={this.handleChange}>
-                    <option selected>Choose school</option>
-                    <option>Paris</option>
-                    <option>Saint-Quentin en Yvelines</option>
-                    <option>Marne-la-Vallée</option>
-                    </select>
-                </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="InputLastName">Last name</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            id="inputLname"
+                            placeholder="Last name"
+                            onChange={this.handleChange}
+                        />
+                    </div>
 
-                <div className="form-group col-md-4" >
-                    <label htmlFor="inputState">Team</label>
-                    <select id="inputLocation" className="form-control" value={this.state.value} onChange={this.handleChange}>
-                    <option selected>Choose a team</option>
-                    <option>Team 1</option>
-                    <option>Team 2</option>
-                    <option>Team 3</option>
-                    </select>
-                </div>
-        
-                 
-        
-                  
-        
-                  <button type="submit" className="btn btn-primary" onClick={this.addToFormulaire}>Register</button>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputEmail">Email address</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="inputEmail"
+                            aria-describedby="emailHelp"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputPassword">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="inputPassword"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputState">School</label>
+                        <select
+                            id="inputLocation"
+                            className="form-control"
+                            onChange={this.handleChange}
+                        >
+                            <option selected>Choose school</option>
+                            <option>Paris</option>
+                            <option>Saint-Quentin en Yvelines</option>
+                            <option>Marne-la-Vallée</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group col-md-4">
+                        <label htmlFor="inputState">Team</label>
+                        <select
+                            id="inputLocation"
+                            className="form-control"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        >
+                            <option selected>Choose a team</option>
+                            <option>Team 1</option>
+                            <option>Team 2</option>
+                            <option>Team 3</option>
+                        </select>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        onClick={this.addToFormulaire}
+                    >
+                        Register
+                    </button>
                 </form>
             </div>
-            );
-        }
-        }
+        );
+    }
+}
 
-        /*function register(e){
+/*function register(e){
             e.preventDefault();
             let request = {
               fname : document.getElementById('inputFname').value,
@@ -110,11 +140,7 @@ class Inscription extends Component{
             })
   };*/
 
-        
-  
-
 export default Inscription;
-
 
 /*
 
